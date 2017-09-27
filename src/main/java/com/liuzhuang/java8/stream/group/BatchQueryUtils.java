9e2a0fa246721a143f5bc89f7df874a1a2d5bd15
xiezx <xiezc@gold-finance.com.cn>
2017-09-27 14:29:54 +0800

    错误异常

--- src/main/java/com/liuzhuang/java8/stream/group/BatchQueryUtils.java
+++ src/main/java/com/liuzhuang/java8/stream/group/BatchQueryUtils.java
@@ -9,6 +9,12 @@ import java.util.stream.Collectors;
 
 /**
  * Created by liuzz on 2017/9/27.
+ *
+ *
+ * Caused by: com.mysql.jdbc.PacketTooBigException: Packet for query is too large (5517669 > 4194304).
+ * You can change this value on the server by setting the max_allowed_packet' variable.
+ *
+ * 分批查询，确保一次的数据量不超过packet大小
  */
 public class BatchQueryUtils {
     private static Logger log = LoggerFactory.getLogger(BatchQueryUtils.class);
